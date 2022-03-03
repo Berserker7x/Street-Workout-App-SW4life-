b16b8b8bc9360007d64dd65d535b311a4c50db4c
thug875d <yassirouaz@gmail.com>
2022-03-03 14:26:24 +0100

    responsive get started

--- app/src/main/java/com/example/firstapp/SlideActivity.java
+++ app/src/main/java/com/example/firstapp/SlideActivity.java
@@ -17,26 +17,11 @@ public class SlideActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_slide);
-        viewPager=findViewById(R.id.viewpager);
-        adapter=new SlideViewPage(this);
+        viewPager = findViewById(R.id.viewpager);
+        adapter = new SlideViewPage(this);
         viewPager.setAdapter(adapter);
-        if(isOpenAlready()){
-            Intent intent=new Intent(SlideActivity.this,MainActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
-          startActivity(intent);
-        }
-        else{
-            SharedPreferences.Editor editor=   getSharedPreferences("slide",MODE_PRIVATE).edit();
-            editor.putBoolean("slide",true);
-            editor.commit();
 
-        }
-
-    }
-    private  boolean isOpenAlready(){
-        SharedPreferences sharedPreferences=getSharedPreferences("slide",MODE_PRIVATE);
-        Boolean result=sharedPreferences.getBoolean("slide",false);
-        return  result;
 
     }
 }
+
