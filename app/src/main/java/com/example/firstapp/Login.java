a3c61b33da1582efa0bf5a58be5d7283492a50dc
thug875d <yassirouaz@gmail.com>
2022-03-21 18:37:33 +0100

    signup google

--- app/src/main/java/com/example/firstapp/Login.java
+++ app/src/main/java/com/example/firstapp/Login.java
@@ -1,6 +1,7 @@
 package com.example.firstapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
@@ -14,6 +15,20 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -27,6 +42,9 @@ public class Login extends AppCompatActivity {
    EditText password;
     Button login;
     Button sign;
+    private FirebaseAuth mAuth;
+    FloatingActionButton Google;
+    private  static  final int RC_SIGN_IN=100;
 
 
 
@@ -40,6 +58,40 @@ public class Login extends AppCompatActivity {
         password=findViewById(R.id.pass);
         login=findViewById(R.id.loginbtn);
         sign=findViewById(R.id.Signupbtn);
+        Google=findViewById(R.id.fab_google);
+
+
+
+        // taking FirebaseAuth instance
+        mAuth = FirebaseAuth.getInstance();
+        //configuration google
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        Google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(Login.this, gso);
+                Intent intent3=mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(intent3,RC_SIGN_IN);
+
+
+
+
+
+            }
+
+
+
+        });
+
+
+
+
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -92,21 +144,77 @@ public class Login extends AppCompatActivity {
 
 
 
+
+
         sign.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent2=new Intent(Login.this,Sign.class);
+                                    @Override
+                                    public void onClick(View v) {
+                                        Intent intent2 = new Intent(Login.this, Sign.class);
 
-                intent2.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
-                 startActivity(intent2);
+                                        intent2.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent2);
 
-            }
-        });
-
+                                    }
 
 
+                                }
+        );
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    if(requestCode==RC_SIGN_IN){
+         Task<GoogleSignInAccount> accountTask=GoogleSignIn.getSignedInAccountFromIntent(data);
+         try{
+             //google Sign auth with firebase
+             GoogleSignInAccount account=accountTask.getResult(ApiException.class);
+             firebaseAuthWithGoogleAccount(account);
+
+         }catch (Exception e){
+
+         }
+        }
+    }
+
+    private void firebaseAuthWithGoogleAccount(GoogleSignInAccount account) {
+    AuthCredential credential=GoogleAuthProvider.getCredential(account.getIdToken(),null);
+    mAuth.signInWithCredential(credential)
+            .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    //login success
+                    //get loged in user
+                    FirebaseUser firebaseUser=mAuth.getCurrentUser();
+                    String uid=mAuth.getUid();
+                    String email=firebaseUser.getEmail();
+                    //check if new or exist
+                    if(authResult.getAdditionalUserInfo().isNewUser()){
+                        //user is new account
+                        Toast.makeText(Login.this,"user creeated",Toast.LENGTH_SHORT).show();
 
 
+                    }else {
+                        //exist
+                        Toast.makeText(Login.this," Existing user ",Toast.LENGTH_SHORT).show();
+                    }
+                    //start profile activity
+                     startActivity(new Intent(Login.this,MainActivity.class));
+                     finish();
+
+
+
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure( Exception e) {
+                    //login failed
+
+
+                }
+            });
 
     }
 }
--- app/src/main/java/com/example/firstapp/Sign.java
+++ app/src/main/java/com/example/firstapp/Sign.java
@@ -34,10 +34,10 @@ public class Sign extends AppCompatActivity {
 
     DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference();
 
-     private   EditText email;
-     private  EditText password;
-     private    EditText username;
-     private Button signup;
+    private   EditText email;
+    private  EditText password;
+    private    EditText username;
+    private Button signup;
     private FirebaseAuth mAuth;
 
 
@@ -60,74 +60,74 @@ public class Sign extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signuptab);
 
-         TextView text = (TextView) findViewById(R.id.spinnerTextView);
+        TextView text = (TextView) findViewById(R.id.spinnerTextView);
         ImageView imageView =(ImageView)findViewById(R.id.spinnerImages);
-         Spinner spinner = (Spinner) findViewById(R.id.mySpinner);
-       SpinnerAdapter adapter = new SpinnerAdapter(this, R.layout.spinner_value_layout, textArray, imageArray);
+        Spinner spinner = (Spinner) findViewById(R.id.mySpinner);
+        SpinnerAdapter adapter = new SpinnerAdapter(this, R.layout.spinner_value_layout, textArray, imageArray);
         spinner.setAdapter(adapter);
         // taking FirebaseAuth instance
         mAuth = FirebaseAuth.getInstance();
 
-       email=findViewById(R.id.email);
+        email=findViewById(R.id.email);
         password=findViewById(R.id.pass);
         username=findViewById(R.id.username);
         signup=findViewById(R.id.button2);
 
         String  categorie = spinner.getSelectedItem().toString();
         signup.setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
-               final String emailvalue=email.getText().toString();
-               final    String passwordvalue=password.getText().toString();
-               final String usermamevalue=username.getText().toString();
-                String  categorie = spinner.getSelectedItem().toString();
+                                      @Override public void onClick(View v) {
+                                          final String emailvalue=email.getText().toString();
+                                          final    String passwordvalue=password.getText().toString();
+                                          final String usermamevalue=username.getText().toString();
+                                          String  categorie = spinner.getSelectedItem().toString();
 
-               if(emailvalue.isEmpty() || passwordvalue.isEmpty() || usermamevalue.isEmpty() ){
-                   Toast.makeText(Sign.this,"Please fill all fields",Toast.LENGTH_SHORT).show();
+                                          if(emailvalue.isEmpty() || passwordvalue.isEmpty() || usermamevalue.isEmpty() ){
+                                              Toast.makeText(Sign.this,"Please fill all fields",Toast.LENGTH_SHORT).show();
 
-               }
-               else{
+                                          }
+                                          else{
 
-                   databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
-                       @Override
-                       public void onDataChange( DataSnapshot snapshot) {
-                           //check if username used
-                           if(snapshot.hasChild(usermamevalue)){
-                               Toast.makeText(Sign.this,"Username is already Registred chose an other one ",Toast.LENGTH_SHORT).show();
+                                              databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                                                  @Override
+                                                  public void onDataChange( DataSnapshot snapshot) {
+                                                      //check if username used
+                                                      if(snapshot.hasChild(usermamevalue)){
+                                                          Toast.makeText(Sign.this,"Username is already Registred chose an other one ",Toast.LENGTH_SHORT).show();
 
-                           }
-                           else {
-                               //sending data to firebase
-                               databaseReference.child("users").child(usermamevalue).child("username").setValue(usermamevalue);
-                               databaseReference.child("users").child(usermamevalue).child("email").setValue(emailvalue);
-                               databaseReference.child("users").child(usermamevalue).child("password").setValue(passwordvalue);
-                               databaseReference.child("users").child(usermamevalue).child("categorie").setValue(categorie);
-                               Toast.makeText(Sign.this,"User registred successfully",Toast.LENGTH_SHORT).show();
+                                                      }
+                                                      else {
+                                                          //sending data to firebase
+                                                          databaseReference.child("users").child(usermamevalue).child("username").setValue(usermamevalue);
+                                                          databaseReference.child("users").child(usermamevalue).child("email").setValue(emailvalue);
+                                                          databaseReference.child("users").child(usermamevalue).child("password").setValue(passwordvalue);
+                                                          databaseReference.child("users").child(usermamevalue).child("categorie").setValue(categorie);
+                                                          Toast.makeText(Sign.this,"User registred successfully",Toast.LENGTH_SHORT).show();
 
-                               Intent intent3=new Intent(Sign.this,Login.class);
+                                                          Intent intent3=new Intent(Sign.this,Login.class);
 
-                               intent3.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
-                               startActivity(intent3);
-
-
-                           }
-                       }
-
-                       @Override
-                       public void onCancelled(  DatabaseError error) {
-
-                       }
-                   });
+                                                          intent3.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
+                                                          startActivity(intent3);
 
 
+                                                      }
+                                                  }
 
-               }
+                                                  @Override
+                                                  public void onCancelled(  DatabaseError error) {
+
+                                                  }
+                                              });
 
 
 
+                                          }
 
 
-            }
-        }
+
+
+
+                                      }
+                                  }
 
         );
 
